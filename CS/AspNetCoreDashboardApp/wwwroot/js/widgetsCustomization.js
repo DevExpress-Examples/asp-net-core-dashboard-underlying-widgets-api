2150e69e19c9166e8c42f0f5865b8afaf7e65a15
Natalia Kazakova <natalia.kazakova@devexpress.com>
2022-11-18 19:28:29 +0400

    add gauges

--- CS/AspNetCoreDashboardApp/wwwroot/js/widgetsCustomization.js
+++ CS/AspNetCoreDashboardApp/wwwroot/js/widgetsCustomization.js
@@ -1,34 +1,56 @@
 function onBeforeRender(s, e) {
     var viewerApiExtension = s.findExtension('viewer-api');
-    if (viewerApiExtension)
+    if (viewerApiExtension) {
         viewerApiExtension.on('itemWidgetOptionsPrepared', customizeWidgetOptions);
+        viewerApiExtension.on('itemWidgetUpdated', customizeWidget);
+        viewerApiExtension.on('itemWidgetCreated', customizeWidget);
+    }
 }
 function customizeWidgetOptions(e) {
     if (e.dashboardItem instanceof DevExpress.Dashboard.Model.GridItem) {
         e.options.hoverStateEnabled = true
     };
     if (e.dashboardItem instanceof DevExpress.Dashboard.Model.ChartItem) {
-        e.options.tooltip = {
-            enabled: false
-        };
+        e.options.tooltip.enabled = false;
         e.options.animation = {
+            ...e.options.animation,
             enabled: true,
             duration: 1000
         };
         e.options.onArgumentAxisClick = function (info) {
-            info.component.getAllSeries()[0].getPointsByArg(info.argument)[0].showTooltip()
+            info.component.getAllSeries()[0].getPointsByArg(info.argument)[0].showTooltip();
         }
     };
     if (e.dashboardItem instanceof DevExpress.Dashboard.Model.PieItem) {
         e.options.legend = {
+            ...e.options.legend,
             visible: true,
             border: {
+                ...e.options.legend.border,
                 visible: true
             }
         };
         e.options.animation = {
+            ...e.options.animation,
             enabled: true,
             duration: 1000
         };
+    };
+    if (e.dashboardItem instanceof DevExpress.Dashboard.Model.GaugeItem) {
+        var gaugesCollection = e.dashboardItem.gauges();
+        gaugesCollection.forEach(element => {
+            if (element.actualValue().dataMember() == 'ExtendedPrice') {
+                e.options.scale.tick.tickInterval = 1000
+            }
+        });
+    };
+}
+function customizeWidget(e) {
+    if (e.dashboardItem instanceof DevExpress.Dashboard.Model.GaugeItem) {
+        var gaugesCollection = e.getWidget();
+        gaugesCollection.forEach(element => {
+            element.option('scale.label.font.weight', '600');
+
+        });
     }
 }
